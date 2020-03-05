0cd101a67813345bca0667f759b92eec74bebdd7
Andrew Lobban <andrewglobban@gmail.com>
2020-03-05 00:24:43 -0600

    Update layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxill';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
 );
+
+export default layout;
