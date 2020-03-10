fe4db7b350794fcdfaa3045bcccb823e100bb528
Andrew Lobban <andrew.lobban@globaltranz.com>
2020-03-10 07:28:15 -0500

    Render Burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxill';
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
   render() {
     return(
       <Aux>
-        <div>Burger</div>
+        <Burger />
         <div>Burger Controls</div>
       </Aux>
     );
